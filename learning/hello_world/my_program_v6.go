9f1aaeb5120db4e817ca2e46fdd2c87bd171d928
Alexey Medov <amedov@gitlab.int>
2022-01-03 22:48:33 +0300

    fix

---
+++ learning/hello_world/my_program_v6.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("-------")
+	fmt.Println("Hello, \nGo!")
+	fmt.Println("-------")
+	fmt.Println("Hello, \tGo!")
+	fmt.Println("-------")
+	fmt.Println("Quotes: \"\"")
+	fmt.Println("-------")
+	fmt.Println("Backslash: \\")
+	fmt.Println("-------")
+}
