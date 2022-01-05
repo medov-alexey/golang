d80bbf969c2da47b610f1e2e734d0e387ff20292
Alexey Medov <amedov@gitlab.int>
2022-01-05 12:26:15 +0300

    fix

---
+++ learning/hello_world/my_program_v7.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+        fmt.Println("\nСимвол и его кодовое обозначение в таблице кодировок Unicode:\n")
+	fmt.Println("A -",'A')
+	fmt.Println("B -",'B')
+	fmt.Println("Ж -",'Ж')
+	fmt.Println("\\t -",'\t',"(табуляция)")
+	fmt.Println("\\n -",'\n',"(переход на новую строку)")
+	fmt.Println("\\ -",'\\',"(обратный слэш)")
+}
