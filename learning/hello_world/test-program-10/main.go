9f34c652b307945ce60b2167b2279fc867a0d329
Alexey Medov <amedov@gitlab.int>
2022-01-07 00:02:33 +0300

    fix

---
+++ learning/hello_world/test-program-10/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("4 < 6", 4 < 6)
+	fmt.Println("4 > 6", 4 > 6)
+	fmt.Println("2+2 == 5", 2+2 == 5)
+	fmt.Println("2+2 != 5", 2+2 != 5)
+	fmt.Println("4 <= 6", 4 <= 6)
+	fmt.Println("4 >= 4", 4 >= 4)
+}
